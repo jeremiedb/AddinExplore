60f223e45a7d12fad126f1cc1b47202c7a19b7b4
jeremiedb <jeremie.desgagne.bouchard@gmail.com>
2017-10-03 03:50:30 -0400

    Update new dplyr and multiple targets

--- R/aggregate_stats.R
+++ R/aggregate_stats.R
@@ -5,158 +5,153 @@
 #'@export
 aggregate_stats <- function() {
 
-  objects_list<- ls(envir = .GlobalEnv)
-  data_list<- objects_list[sapply(objects_list, function(x) "data.frame" %in% class(get(x, envir = .GlobalEnv)))]
+  objects_list <- ls(envir = .GlobalEnv)
+  if (length(objects_list) == 0) {
+    shiny::stopApp()
+    message("no object in environment")
+  }
 
+  data_list <- objects_list[sapply(objects_list, function(x) "data.frame" %in% class(get(x, envir = .GlobalEnv)))]
+  if (length(data_list) == 0) {
+    shiny::stopApp()
+    message("no data.frame in environment")
+  }
   ui <- miniUI::miniPage(
     miniUI::gadgetTitleBar("Exploration"),
 
     miniUI::miniButtonBlock(
       shiny::selectInput("list_data", label = "Input Data", choices = data_list, multiple = F),
-      shiny::selectInput("target_var", label = "Target", choices = "", multiple = F, selectize = T),
-      shiny::selectInput("compare_var", label = "Compare", choices = "", multiple = F, selectize = T),
-      shiny::selectInput("expo_var", label = "Exposure", choices = "", multiple = F),
+      shiny::selectInput("targets_var", label = "Targets", choices = "", selected = "", multiple = T, selectize = T),
+      shiny::selectInput("weight_var", label = "Weight", choices = "", multiple = F),
       shiny::selectInput("group_var", label = "Group", choices = "", multiple = F),
-      shiny::numericInput("num_bins", label = "Bins #", value = 10, min = 1, max = 128, step = 1),
+      shiny::numericInput("num_bins", label = "Bins #", value = 10, min = 1, max = 256, step = 1),
       border = "bottom"),
 
-    shiny::fillRow(
-      plotly::plotlyOutput("plot")
+    miniTabstripPanel(
+      miniTabPanel("Graph", icon = shiny::icon("area-chart"),
+                   shiny::fillRow(
+                     plotly::plotlyOutput("plot", height = "100%")
+                   )),
+      miniTabPanel("Data", icon = shiny::icon("table"),
+                   miniContentPanel(
+                     DT::dataTableOutput("table", height = "100%")
+                   ))
     )
   )
 
   server <- function(input, output, session) {
 
-    data_input<- reactive({
-      data_select<- input$list_data
+    data_input <- reactive({
+      data_select <- input$list_data
       if (!data_select=="") {
-        raw_data<- get(data_select, envir = .GlobalEnv)
-      } else raw_data<- data.frame(xx=1:5, yy=1:5, zz=1:5)
-      vars<-names(raw_data)
-      var_types<-sapply(raw_data, class)
+        raw_data <- get(data_select, envir = .GlobalEnv)
+      } else raw_data <- data.frame(xx=1:5, yy=1:5, zz=1:5)
+      vars <- names(raw_data)
+      var_types <- sapply(raw_data, class)
       list(data=raw_data, vars=vars, var_types=var_types)
     })
 
     observe({
-      data_select<- input$list_data
-      var_list<- data_input()$vars
-      var_types<- data_input()$var_types
+      var_list <- data_input()$vars
+      var_types <- data_input()$var_types
+      num_vars <- names(var_types)[var_types %in% c("numeric", "integer")]
 
-      num_vars<- names(var_types)[var_types %in% c("numeric", "integer")]
-
-      shiny::updateSelectizeInput(session, inputId = "target_var", choices = num_vars)
-      shiny::updateSelectizeInput(session, inputId = "compare_var", choices = num_vars)
-      shiny::updateSelectInput(session, inputId = "expo_var", choices = num_vars)
+      shiny::updateSelectizeInput(session, inputId = "targets_var", choices = num_vars, selected = num_vars[1])
+      shiny::updateSelectInput(session, inputId = "weight_var", choices = num_vars)
       shiny::updateSelectInput(session, inputId = "group_var", choices = var_list)
     })
 
     ### Generate plot
     observe({
 
-      #data_select<- input$list_data
-      target_var<- input$target_var
-      compare_var<- input$compare_var
-      expo_var<- input$expo_var
-      group_var<- input$group_var
-      num_bins<- input$num_bins
+      data_select <- input$list_data
+      targets_var <- input$targets_var
+      weight_var <- input$weight_var
+      group_var <- input$group_var
+      num_bins <- input$num_bins
 
-      var_types<- data_input()$var_types
-      num_vars<- names(var_types)[var_types %in% c("numeric", "integer")]
+      var_types <- data_input()$var_types
+      num_vars <- names(var_types)[var_types %in% c("numeric", "integer")]
 
-      data_raw<- data_input()$data
+      data_raw <- data_input()$data
 
       ### Patch for no selected target
-      if (is.na(target_var) | target_var==""){
-        target_var<- num_vars[1]
+      if (is.null(targets_var)) {
+        targets_var <- num_vars[1]
       }
-
-      ### Patch for no selected compare
-      if (is.na(compare_var) | compare_var==""){
-        compare_var<- num_vars[1]
+      if (anyNA(targets_var) | targets_var[1]=="") {
+        targets_var <- num_vars[1]
       }
 
       ### Patch to assume vector of 1s if no weight is specified
-      if (is.na(expo_var) | expo_var==""){
-        expo_var<- "AllOnes"
-        data_raw$AllOnes<-1
+      if (is.na(weight_var) | weight_var==""){
+        weight_var <- "AllOnes"
+        data_raw$AllOnes <-1
       }
 
       ### Patch for no selected group
       if (is.na(group_var) | group_var==""){
-        group_var<- "TOTAL"
-        data_raw$TOTAL<- "TOTAL"
+        group_var <- "TOTAL"
+        data_raw$TOTAL <- "TOTAL"
       }
 
       ### Patch to reset variables when changing dataset
-      if (!all(c(target_var, compare_var, expo_var, group_var) %in% colnames(data_raw))){
-        target_var<- num_vars[1]
-        compare_var<- num_vars[1]
-        expo_var<- num_vars[1]
-        group_var<- num_vars[1]
+      if (!all(c(targets_var, weight_var, group_var) %in% colnames(data_raw))){
+        targets_var <- num_vars[1]
+        weight_var <- num_vars[1]
+        group_var <- num_vars[1]
       }
 
+      data_raw[, c(".group", ".weight")] <- data_raw[, c(group_var, weight_var)]
+
+      ### bin numeric variable
+      if (group_var %in% num_vars & !is.na(num_bins) & !all(data_raw$.group[1]==data_raw$.group)){
+        data_raw$.group <- cut(data_raw$.group, breaks = sort(unique(quantile(data_raw$.group, probs = 0:num_bins/num_bins))), include.lowest = T)
+      }
+
+      # Exposure approach
+      data_sum <- data_raw %>%
+        dplyr::group_by(.group) %>%
+        dplyr::summarise_at(.vars = c(targets_var, ".weight"), .funs = dplyr::funs(sum(as.numeric(.), na.rm=T))) %>%
+        dplyr::mutate_at(.vars = targets_var, .funs = funs(./.weight))
+
+      output$table<- DT::renderDataTable({
+        DT::datatable(data = data_sum, class = "display compact", rownames = F, filter = "none",
+                      options = list (
+                        paging = T,
+                        pageLength = 10,
+                        lengthMenu = c(5, 10, 25, 50, 100),
+                        lengthChange = T,
+                        searching = F,
+                        ordering = F,
+                        info = F
+                      ))
+      })
 
       output$plot<- plotly::renderPlotly({
         if (!is.null(group_var) & !group_var==""){
 
-          data_raw<- data_raw %>%
-            mutate_(target=formula(paste0("~",target_var)),
-                    compare=formula(paste0("~",compare_var)),
-                    expo=formula(paste0("~",expo_var)),
-                    group=formula(paste0("~",group_var)))
+          p <- plotly::plot_ly(data = data_sum, x=~.group , y=~.weight, xaxis = "x", yaxis = "y", type="bar", color=I("lightblue"), alpha = 0.5, name=weight_var)
 
-          # ### bin numeric variable
-          if (group_var %in% num_vars & !is.na(num_bins) & !all(data_raw$group[1]==data_raw$group)){
-            data_raw$group<- cut(data_raw$group, breaks = sort(unique(quantile(data_raw$group, probs = 0:num_bins/num_bins))), include.lowest = T)
+          for (traces in targets_var) {
+            p <- plotly::add_trace(p = p, data = data_sum, x=~.group , y = as.formula(paste0("~", traces)), xaxis = "x", yaxis="y2", type="scatter", mode="lines+markers", name=traces, alpha=1, inherit = F)
           }
-
-          data_sum<- data_raw %>%
-            dplyr::group_by(group) %>%
-            dplyr::summarise_at(.cols = dplyr::vars(target, compare, expo), .funs = dplyr::funs(sum(as.numeric(.), na.rm=T))) %>%
-            dplyr::mutate(ratio_target=target/expo,
-                          ratio_compare=compare/expo)
-
-          plotly::plot_ly(data_sum, x=~group , y=~expo, type="bar", color=I("lightblue"), alpha = 0.5, name="Exposure") %>%
-            plotly::add_trace(data_sum, x=~group , y=~ratio_compare, yaxis="y2", type="scatter", mode="lines+markers", color=I("darkgrey"), name="Compare", alpha=1) %>%
-            plotly::add_trace(data_sum, x=~group , y=~ratio_target, yaxis="y2", type="scatter", mode="lines+markers", color=I("navy"), name="Target", alpha=1) %>%
-            plotly::layout(
-              xaxis=list(
-                title = group_var
-              ),
-              yaxis=list(
-                title = expo_var
-              ),
-              yaxis2=list(
-                overlaying = "y",
-                side = "right",
-                title = paste0(target_var, "/", expo_var)
-              ),
-              margin=list(b=120, l=50, r=100)
-            ) %>%
-            plotly::config(collaborate = F, editable=F, showLink=F, sendData=F, displaylogo=T)
-
-
-          # data_sum<- data_raw %>% dplyr::group_by_(.dots = group_var) %>%
-          #   dplyr::summarise_at(.cols = c(target_var,expo_var), .funs = dplyr::funs(sum(., na.rm=T))) %>%
-          #   dplyr::mutate_(ratio=paste0(target_var, "/", expo_var))
-          #
-          # plotly::plot_ly(data_sum, x=formula(paste0("~", group_var)) , y=formula(paste0("~", expo_var)), type="bar", color=I("gray"), alpha = 0.5, name=expo_var) %>%
-          #   plotly::add_trace(data_sum, x=formula(paste0("~", group_var)) , y=~ratio, yaxis="y2", type="scatter", mode="lines+markers", color=I("navy"), name="ratio", alpha=1) %>%
-          #   plotly::layout(
-          #     xaxis=list(
-          #       title = group_var
-          #     ),
-          #     yaxis=list(
-          #       title = paste0(target_var, "/", expo_var)
-          #     ),
-          #     yaxis2=list(
-          #       overlaying = "y",
-          #       side = "right",
-          #       title = "ratio"
-          #     ),
-          #     margin=list(b=120)
-          #   ) %>%
-          #   plotly::config(collaborate = F, editable=F, showLink=F, sendData=F, displaylogo=T)
+          p <- p %>% plotly::layout(
+            xaxis=list(
+              title = group_var
+            ),
+            yaxis=list(
+              side = "right",
+              title = "Weight"
+            ),
+            yaxis2=list(
+              overlaying = "y",
+              side = "left",
+              title = "Targets"
+            ),
+            margin=list(b=120, l=50, r=100)
+          ) %>%
+            plotly::config(collaborate = F, editable=F, showLink=F, sendData=F, displaylogo=F)
 
         } else plotly::plotly_empty(type="scatter", mode="lines")
       })
@@ -169,9 +164,9 @@ aggregate_stats <- function() {
     })
   }
 
-  viewer <- shiny::dialogViewer(dialogName = "Explorer", width=900, height=1200)
-  #viewer <- shiny::browserViewer()
-  #viewer <- shiny::paneViewer(minHeight = 300)
+  viewer <- shiny::browserViewer()
+  # viewer <- shiny::dialogViewer(dialogName = "Explorer", width=900, height=1200)
+  # viewer <- shiny::paneViewer(minHeight = 300)
 
   shiny::runGadget(ui, server, viewer = viewer)
 }
